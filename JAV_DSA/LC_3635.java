278ed9720ca37caf35f1335f3797c07cab30ddca
sahilsahoo-DIZIXION <sahilsahoo396@gmail.com>
2026-06-03 20:27:12 +0530

    LC 3635

---
+++ JAV_DSA/LC_3635.java
@@ -0,0 +1,18 @@
+class Solution{
+    public int earliestFinishTime(int[] landStartTime,int[] landDuration,int[] waterStartTime,int[] waterDuration){
+        int a=m(landStartTime,landDuration,waterStartTime,waterDuration);
+        int b=m(waterStartTime,waterDuration,landStartTime,landDuration);
+        return Math.min(x,y);
+    }
+    public int m(int[] s1,int[] d1,int[] s2,int[] d2){
+        int minEnd=Integer.MAX_VALUE;
+        for(int i=0;i<s1.length;i++){
+            minEnd=Math.min(minEnd,s1[i]+d1[i]);
+        }
+        int ans=Integer.MAX_VALUE;
+        for(int i=0;i<s2.length;i++){
+            ans=Math.min(ans,Math.max(minEnd,s2[i])+d2[i]);
+        }
+        return ans;
+    }
+}
