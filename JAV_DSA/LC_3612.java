88a336af433b039986d3b428620612bc18835bc4
sahilsahoo-DIZIXION <sahilsahoo396@gmail.com>
2026-06-16 23:07:56 +0530

    LC 3612

---
+++ JAV_DSA/LC_3612.java
@@ -0,0 +1,19 @@
+class Solution{
+    public String processStr(String s){
+        StringBuilder ans=new StringBuilder();
+        for(char ch:s.toCharArray()){
+            if(Character.isLowerCase(ch)){
+                ans.append(ch);
+            }else if(ch=='*'){
+                if(ans.length()>0){
+                    ans.deleteCharAt(ans.length()-1);
+                }
+            }else if(ch=='#'){
+                ans.append(ans);
+            }else{
+                ans.reverse();
+            }
+        }
+        return ans.toString();
+    }
+}
